d4d0e45087bead86abeafc286edcc826438749b7
vinaya sagar duba <vinayduba@gmail.com>
2025-08-25 10:31:25 +0530

    extract reponse and store to a variable

--- src/test/java/Assertion4SomeFields.java
+++ src/test/java/Assertion4SomeFields.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -9,10 +11,17 @@ public class Assertion4SomeFields {
 
 	public static void main(String[] args) {
 		RestAssured.baseURI="https://rahulshettyacademy.com/";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 			   .body(ReqPay.payld())
 		.when().post("maps/api/place/add/json/")
-		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server","Apache/2.4.52 (Ubuntu)");
+		.then().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server","Apache/2.4.52 (Ubuntu)")
+		.extract().response().asString();
+		
+		System.out.println(response);
+		JsonPath js=new JsonPath(response);
+		System.out.println();
+		String place_id=js.getString("place_id");
+		System.out.println(place_id);
 		
 
 	}
