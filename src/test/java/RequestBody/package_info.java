9a5fab3c8506af6533db7f1709eb732a7b9deddd
vinaya sagar duba <vinayduba@gmail.com>
2025-08-25 09:42:26 +0530

    added assertions to check the field values

---
+++ src/test/java/RequestBody/package_info.java
@@ -0,0 +1,22 @@
+package RequestBody;
+public class package_info{
+	public static String request()
+	{
+		return "{\r\n"
+				+ "    \"location\": {\r\n"
+				+ "        \"lat\": -38.38349112,\r\n"
+				+ "        \"lng\": 33.42736112\r\n"
+				+ "    },\r\n"
+				+ "    \"accuracy\": 555112,\r\n"
+				+ "    \"name\": \"Frontline house\",\r\n"
+				+ "    \"phone_number\": \"(+91) 983 893 4112\",\r\n"
+				+ "    \"address\": \"29, side layout, cohen 122\",\r\n"
+				+ "    \"types\": [\r\n"
+				+ "        \"Fruit Shop112\",\r\n"
+				+ "        \"shop112\"\r\n"
+				+ "    ],\r\n"
+				+ "    \"website\": \"https://rahulshettyacademy.com\",\r\n"
+				+ "    \"language\": \"French-IN\"\r\n"
+				+ "}";
+	}
+}
--- src/test/java/com/vinay/qa/Rest_Assured_Framework/DataValidation.java
+++ src/test/java/com/vinay/qa/Rest_Assured_Framework/DataValidation.java
@@ -2,17 +2,21 @@ package com.vinay.qa.Rest_Assured_Framework;
 
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 import RequestBody.package_info;
 
-public class KnowledgeCheck {
+public class DataValidation {
 
 	public static void main(String[] args) {
 		RestAssured.baseURI="https://rahulshettyacademy.com/";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		.body(package_info.request())
 		.when().post("maps/api/place/add/json/")
-		.then().log().all().assertThat().statusCode(200);
+		.then().log().all().assertThat().statusCode(200)
+		.body("scope", equalTo("APP"))
+		.header("Server", "Apache/2.4.52 (Ubuntu)");
+
 	}
 
 }
